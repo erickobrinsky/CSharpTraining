1e6dccfb42188d6045fd7413e38ddfaa80e27929
Eric Kobrinsky <Eric.Kobrinsky@allscripts.com>
2024-01-15 12:16:04 -0300

    Delegates and basic advanced C# concepts

---
+++ HRAdministrationAPI/EmployeeBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAdministrationAPI
+{
+    public class EmployeeBase : IEmployee
+    {
+        public int Id { get; set ; }
+        public string FirstName { get ; set ; }
+        public string LasttName { get ; set ; }
+        public virtual decimal Salary { get ; set ; } //. When you declare a member (such as a property, method, or event) as virtual, you are indicating that this member's behavior can be overridden in derived classes.
+    }
+}
---
+++ HRAdministrationAPI/IEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAdministrationAPI
+{
+    public interface IEmployee //All members of an interface are implicitly public, so don't need to declare public or private
+                        // the purpose of an interface is to provide a public contract that other types can implement
+    {
+         int Id { get; set; }
+
+         string FirstName { get; set; }
+
+         string LasttName { get; set; }
+
+         decimal Salary { get; set; }
+    }
+}
